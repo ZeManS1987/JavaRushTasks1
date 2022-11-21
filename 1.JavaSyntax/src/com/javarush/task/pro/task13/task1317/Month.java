cba130fea277181c014b41a35e0591ed70dcc826
ZeManS1987 <93866994+ZeManS1987@users.noreply.github.com>
2022-11-22 00:06:08 +0300

    Новый тип данных – enum: перечисление

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1317/Month.java
@@ -0,0 +1,42 @@
+package com.javarush.task.pro.task13.task1317;
+
+/* 
+Трансформируем enum в класс
+*/
+
+public class Month {
+
+    public static void main(String[] args) {
+
+    }
+    public static final Month JANUARY = new Month(0);
+    public static final Month FEBRUARY = new Month(1);
+    public static final Month MARCH = new Month(2);
+    public static final Month APRIL = new Month(3);
+    public static final Month MAY = new Month(4);
+    public static final Month JUNE = new Month(5);
+    public static final Month JULY = new Month(6);
+    public static final Month AUGUST = new Month(7);
+    public static final Month SEPTEMBER = new Month(8);
+    public static final Month OCTOBER = new Month(9);
+    public static final Month NOVEMBER = new Month(10);
+    public static final Month DECEMBER = new Month(11);
+
+    private static final Month[] array ={JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
+
+    private final int values;
+
+    private Month(int value){
+        this.values = value;
+    }
+
+    public int ordinal(){
+      return   this.values;
+    }
+
+    public static Month[] values(){
+        return array;
+    }
+
+
+}

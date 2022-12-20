cf710394c20dc12936988b8c2d46e40a17247444
ZeManS1987 <93866994+ZeManS1987@users.noreply.github.com>
2022-12-20 23:43:11 +0300

    Класс Date

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1601/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task16.task1601;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/* 
+Лишь бы не в понедельник :)
+*/
+
+public class Solution {
+
+    static Date birthDate = new Date(87,Calendar.JANUARY, 15);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Date date) {
+        //напишите тут ваш код
+        String result = "";
+        int day = date.getDay();
+        return switch (day) {
+            case 0 -> "воскресенье";
+            case 1 -> "понедельник";
+            case 2 -> "вторник";
+            case 3 -> "среда";
+            case 4 -> "четверг";
+            case 5 -> "пятница";
+            case 6 -> "суббота";
+            default -> result;
+        };
+
+    }
+}

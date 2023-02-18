a146e3d46e969994f9bb803ceddebd226814b0ff
ZeManS1987 <93866994+ZeManS1987@users.noreply.github.com>
2023-02-19 00:30:44 +0300

    abstract class

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1709/Shape.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task17.task1709;
+
+/* 
+Абстрактные фигуры
+*/
+
+public abstract class Shape {
+    //напишите тут ваш код
+    private double v;
+    private double i;
+    public abstract double getPerimeter();
+    public abstract double getArea();
+
+
+}
